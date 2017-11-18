9378e5dd4c895b6ba615aeee601ea4c6e662c728
qianshuangbei <qianshuangbei@tju.edu.cn>
2017-11-18 17:56:06 +0800

    add treelist

--- MultiMode/Nanodraw/NanoDraw.cs
+++ MultiMode/Nanodraw/NanoDraw.cs
@@ -8,21 +8,67 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MultiMode.Nanoman;
+using MultiMode.Nanomanipulation;
 
 namespace MultiMode.Nanodraw
 {
     public partial class NanoDraw : Form
     {
+        private uint linenumber;
+        private uint circlenumber;
+        private uint arcnumber;
+        public Patternstruct patterndata;
+        public int linewidthes;
         public NanoDraw()
         {
             InitializeComponent();
+            linenumber = 1;
+            circlenumber = 1;
+            arcnumber = 1;
         }
-        public void add_line_path() {
+        public TreeNode SearchNode(string name) {
+            foreach (TreeNode n in pathTree.Nodes)
+            {
+                if (n.Text == name)
+                {
+                    return n;
+                }
+            }
+             return null;
         }
-        public void delete_line_path()
+        public void add_line_path(PointF pivot, PointF endpoint) {
+            string strtemp = "Line" + linenumber.ToString();
+            TreeNode parentNode = SearchNode("Line");
+            TreeNode treeNode = new TreeNode();
+            treeNode.Name = strtemp;
+            treeNode.Text = strtemp;
+            treeNode.ContextMenuStrip = contextMenuStrip1;
+            parentNode.Nodes.Add(treeNode);
+            patterndata.patternLine.Add(new PointF[2] { pivot, endpoint });
+        }
+        public void add_circle_path(PointF pivot, PointF endpoint)
         {
-
+            string strtemp = "Circle" + circlenumber.ToString();
+            TreeNode parentNode = SearchNode("Circle");
+            TreeNode treeNode = new TreeNode();
+            treeNode.Name = strtemp;
+            treeNode.Text = strtemp;
+            treeNode.ContextMenuStrip = contextMenuStrip1;
+            parentNode.Nodes.Add(treeNode);
+            patterndata.patternCircle.Add(new PointF[2] { pivot, endpoint });
         }
+        public void add_arc_path(PointF pivot, PointF endpoint)
+        {
+            string strtemp = "Arc" + circlenumber.ToString();
+            TreeNode parentNode = SearchNode("Arc");
+            TreeNode treeNode = new TreeNode();
+            treeNode.Name = strtemp;
+            treeNode.Text = strtemp;
+            treeNode.ContextMenuStrip = contextMenuStrip1;
+            parentNode.Nodes.Add(treeNode);
+            patterndata.patternCircle.Add(new PointF[2] { pivot, endpoint });
+        }
+
         private void line_Click(object sender, EventArgs e)
         {
             PushByHand.mouseSelectMode = PushByHand.drawState.DRAWLINE;
@@ -38,10 +84,83 @@ namespace MultiMode.Nanodraw
             PushByHand.mouseSelectMode = PushByHand.drawState.DRAWCIRCLE;
         }
 
-        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        private void contextMenuStrip1_MouseDown(object sender, MouseEventArgs e)
         {
+            TreeNode deletenode = pathTree.SelectedNode;
+            string nodename = deletenode.Text;
+            if (nodename.Contains("Line"))
+            {
+                int nodeindex = Convert.ToInt32(nodename.Substring(4));
+                linenumber--;
+                TreeNode tempnode = SearchNode("Line");
+                int i = 0;
+                foreach (TreeNode n in tempnode.Nodes) {
+                    n.Text = "Line" + i.ToString();
+                }
+                patterndata.patternLine.RemoveAt(nodeindex);
+            }
+            else if (nodename.Contains("Circle")) {
+                int nodeindex = Convert.ToInt32(nodename.Substring(6));
+                circlenumber--;
+                TreeNode tempnode = SearchNode("Circle");
+                int i = 0;
+                foreach (TreeNode n in tempnode.Nodes)
+                {
+                    n.Text = "Line" + i.ToString();
+                }
+                patterndata.patternCircle.RemoveAt(nodeindex);
+            }
+            else if (nodename.Contains("Arc")) {
+                int nodeindex = Convert.ToInt32(nodename.Substring(3));
+                arcnumber--;
+                TreeNode tempnode = SearchNode("Arc");
+                int i = 0;
+                foreach (TreeNode n in tempnode.Nodes)
+                {
+                    n.Text = "Line" + i.ToString();
+                }
+                patterndata.patternCircle.RemoveAt(nodeindex);
+            }
 
+
+            deletenode.Remove();
         }
+
+        private void lineWidthinput_TextChanged(object sender, EventArgs e)
+        {
+            linewidthes = Convert.ToInt32(lineWidthinput.Text);
+        }
+
+        private void Generate_Click(object sender, EventArgs e)
+        {
+            if (patterndata.patternArc.Count > 0)
+            {
+                foreach (PointF[] t in patterndata.patternLine)
+                {
+                    List<PointF> datalist =new List<PointF>(); 
+                    double length = MathCalculate.GetDistance(t[0], t[1]);
+                    double angle = MathCalculate.GetAngleWithDirection(t[0], t[1]);
+                    const double RULE = 0.020;
+                    double density = RULE/(PushByHand._xSize/ PushByHand._sampsInLine);
+                    for (double i =0; i < length; i++)
+                    {
+                        PointF temp1 = new PointF((float)i, (float)i+(float)linewidthes/2);
+                        PointF temp2 = new PointF((float)i, (float)i - (float)linewidthes / 2);
+                        datalist.Add(temp1);
+                        datalist.Add(temp2);
+                        i = i + density;
+                    }
+                    
+                }
+            }
+            if (patterndata.patternLine.Count > 0)
+            {
+            }
+            if (patterndata.patternCircle.Count > 0)
+            {
+            }
+        }
+
         //  public addpattern(List<>)
     }
 }
