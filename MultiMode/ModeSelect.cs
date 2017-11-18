75881b95c91933ac5da8d230c615b7bf4782f443
BYCR7 <baihuitian@tju.edu.cn>
2017-11-18 10:14:01 +0800

    new program

---
+++ MultiMode/ModeSelect.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using MultiMode.Nanomanipulation;
+using MultiMode.Nanoman;
+
+namespace MultiMode
+{
+    public partial class ModeSelect : Form
+    {
+        public static string AFMPicturePath;
+        public ModeSelect()
+        {
+            InitializeComponent();
+            AFMPicturePath = null;
+        }
+
+        private void load_Click(object sender, EventArgs e)
+        {
+            if (nanomanipulation.Checked)
+            {
+                AutoDetect form = new AutoDetect();
+                this.Visible = false;
+                form.ShowDialog();
+                form.Dispose();
+
+                Application.Exit();
+            }
+            else if (nanoman.Checked)
+            {
+                PushByHand form = new PushByHand();
+                this.Visible = false;
+                form.ShowDialog();
+                form.Dispose();
+
+                Application.Exit();
+            }
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ MultiMode/Nanoman/RefreshFigure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MultiMode.Nanoman
+{
+    class RefreshFigure
+    {
+        public static Bitmap BackgroundImageRefresh(Bitmap image)
+        {
+            Graphics g = Graphics.FromImage(image);
+            Pen myPen = new Pen(Color.White, 1);
+            PointF p1, p2;
+            for (int i = 0; i < SavePath.handPath.Count; i += 2)
+            {
+                p1 = new PointF((float)((SavePath.handPath[i].X + PushByHand._xSize / 2) / PushByHand._xSize * PushByHand._sampsInLine),
+                    (float)(PushByHand._numberOfLines - (SavePath.handPath[i].Y + PushByHand._ySize / 2) / PushByHand._ySize * PushByHand._numberOfLines));
+                p2 = new PointF((float)((SavePath.handPath[i + 1].X + PushByHand._xSize / 2) / PushByHand._xSize * PushByHand._sampsInLine),
+                    (float)(PushByHand._numberOfLines - (SavePath.handPath[i + 1].Y + PushByHand._ySize / 2) / PushByHand._ySize * PushByHand._numberOfLines));
+
+                g.DrawLine(myPen, p1, p2);
+            }
+
+            return image;
+        }
+    }
+}
